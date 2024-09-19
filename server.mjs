ffad4f2ee1d8251153ccb3c59d78141f58baed62
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-09-19 10:29:01 +0200

    Create server.mjs

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+ console.log("request received");
+
+ res.statusCode = 200;
+
+ res.setHeader("Content-Type", "application/json");
+
+ const jsonResponseBody = JSON.stringify({ location: "Mars" })
+
+ res.end(jsonResponseBody);
+});
+
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
