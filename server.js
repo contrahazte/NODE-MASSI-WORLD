844f8427ec76d318f4a90c327e7f4283386fe632
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-09-19 10:27:54 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Summer</title>
+    </head>
+    <body>
+      <h1>¡TACHAAAAAAAAAAN!</h1>
+      <p>35ºC y yo sin ventilador O.o' </p>
+    </body>
+    </html>
+  `);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
