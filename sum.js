07627ba0008a33a3acfb741219716fd2ef84433c
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-09-19 10:19:06 +0200

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,13 @@
+let sum = 5 + 6; 
+
+console.log(`Sum result: ${sum}`);
+
+//! node
+// Welcome to Node.js v21.7.3.
+// Type ".help" for more information.
+//! > let sum = 5 + 6; 
+// undefined
+//! > console.log(`The result is: ${sum}`)
+//! The result is: 11
+// undefined
+// >
